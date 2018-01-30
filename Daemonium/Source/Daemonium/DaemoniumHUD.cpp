219a319b92caf56503f2e818845737d8a23541e2
Gavin Keirstead <gmk1072@g.rit.edu>
2018-01-30 16:05:08 -0500

    imported assets changed crosshair, added some things to my level

--- Daemonium/Source/Daemonium/DaemoniumHUD.cpp
+++ Daemonium/Source/Daemonium/DaemoniumHUD.cpp
@@ -10,7 +10,7 @@
 ADaemoniumHUD::ADaemoniumHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/Textures/DotCrosshair"));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
